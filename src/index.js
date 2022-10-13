cc247e568b199913f2145a04e0a00ee2061e5124
Antonio Pereira <anthonyesany0712@gmail.com>
2022-10-13 15:34:27 -0300

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/global.scss';
+import Header from './components/Header';
+
+// jsx
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className='main__wrapper'>
+    <div className='main__container'>
+      <Header/>
+
+    </div>
+
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
