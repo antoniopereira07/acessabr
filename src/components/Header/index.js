000a82b1ed5538b1a5c4e3c4b36dd52d661109ca
Antonio Pereira <anthonyesany0712@gmail.com>
2022-10-13 15:42:01 -0300

    Initialize project using Create React App

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,13 @@ import './style.scss';
 import logoAcessaBr from '../../images/logo-acessaBR.svg'
 import logoEbac from '../../images/logo-ebac.svg'
 
-const Header = props => {
+const Header = () => {
     return(
         <header className="header__container">
             <div className="header__logo header__logo--state">
                 <img src={logoAcessaBr} alt="Logo AcessaBR"/>
                 <span className="header__city">
-                     / {props.city} - {props.state}
+                     / Teresina - PI
                 </span>
             </div>
             <div className="header__logo--ebac">
