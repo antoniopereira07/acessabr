9b13a7ec93eae50164486caf1dddd6e0d7ae3a99
Antonio Pereira <anthonyesany0712@gmail.com>
2022-10-22 22:19:21 -0300

    aplicando context

--- src/index.js
+++ src/index.js
@@ -2,25 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.scss';
 import Header from './components/Header';
-import Home from './pages/SaoPaulo';
+import Home from './pages/Home';
 import Curator from './components/Curator';
 import Footer from './components/Footer';
 import Places from './components/Places';
 import Routes from './routes';
-
-const CONFIG = {
-  state: 'PI',
-  city: 'Teresina'
-}
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div className='main__wrapper'>
     <div className='main__container'>
-      <Header city={CONFIG.city} state={CONFIG.state} />
-      <Routes city={CONFIG.city} state={CONFIG.state} />
-      <Places />
-      <Curator />
+      <BrowserRouter>
+        <Routes />
+        <Places />
+        <Curator />
+      </BrowserRouter>
     </div>
     <Footer />
 
