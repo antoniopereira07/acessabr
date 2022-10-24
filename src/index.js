8749ffa5e9785d941704407f84b54360627bfa48
Antonio Pereira <anthonyesany0712@gmail.com>
2022-10-24 15:04:06 -0300

    ajustes finais

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.scss';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Curator from './components/Curator';
 import Footer from './components/Footer';
-import Places from './components/Places';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -15,11 +11,8 @@ root.render(
     <div className='main__container'>
       <BrowserRouter>
         <Routes />
-        <Places />
-        <Curator />
       </BrowserRouter>
     </div>
     <Footer />
-
   </div>
 );
